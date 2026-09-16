2e8fd17d81a3a102d25dc65541ca232b458b398a
Brainspark1 <165429616+Brainspark1@users.noreply.github.com>
2026-09-16 18:34:03 -0500

    Fixing styling + making not-goofy mode less goofy

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const checkbox = document.getElementById("goofy-checkbox");
 
+const introParagraph = document.getElementById("intro-paragraph");
+
 checkbox.addEventListener('change', function() {
     if (this.checked) {
         document.body.classList.add("goofy-theme");
 
-        console.log("Goofy enabled")
+        console.log("Goofy enabled");
     } else {
         document.body.classList.remove("goofy-theme");
 
-        console.log("Goofy disabled")
+        console.log("Goofy disabled");
     }
 }) 
